a63eb2f31ce9b288d8562027f48148dc4bdd5250
travis-racisz <63677846+travis-racisz@users.noreply.github.com>
2021-05-14 11:05:13 +0900

    Update server.js

--- server.js
+++ server.js
@@ -54,5 +54,7 @@ if (process.env.NODE_ENV === 'production') {
     const path = require('path');
     app.get('*', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-app.listen(process.env.PORT || 8000, () => console.log(`server is running on port ${process.env.PORT}`));
+    })
+}
+
+app.listen(process.env.PORT || 8000, () => console.log(`server is running on port ${process.env.PORT}`))

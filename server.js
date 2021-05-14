6938aa0836b314daa93a9062cf266b7528fb162d
travis-racisz <63677846+travis-racisz@users.noreply.github.com>
2021-05-14 10:59:24 +0900

    changed server to app

--- server.js
+++ server.js
@@ -47,4 +47,4 @@ io.on('connection', socket => {
 
 });
 
-server.listen(process.env.PORT || 8000, () => console.log(`server is running on port ${process.env.PORT}`));
+app.listen(process.env.PORT || 8000, () => console.log(`server is running on port ${process.env.PORT}`));
